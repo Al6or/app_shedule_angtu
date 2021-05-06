17f3e7ef416825f8763409df56ba572256a9ba67
Alexandra <sd-ks-12@yandex.ru>
2021-05-06 21:34:09 +0800

    добавлен свайп в зависемости от недели загружается четная или нечетная на главно странице

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,9 +3,11 @@ import 'package:angtu_shedule_flutter/models/Shedule.dart';
 import 'package:angtu_shedule_flutter/screens/exam.dart';
 import 'package:angtu_shedule_flutter/screens/globals.dart';
 import 'package:angtu_shedule_flutter/screens/info.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:angtu_shedule_flutter/screens/settings.dart';
 import 'package:grouped_list/grouped_list.dart';
+import 'package:week_of_year/week_of_year.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/home";
@@ -67,62 +69,158 @@ class HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  List<SheduleFalse> _group = [];
+//главная страница
+  int _currentIndex = nowNumberWeek();
+  PageController _pageController;
+
+  List<SheduleFalse> _groupFalse = [];
+  List<SheduleFalse> _groupTrue = [];
   bool _loading;
 
   @override
   void initState() {
     super.initState();
+    _pageController = PageController(initialPage: nowNumberWeek());
+
     _loading = true;
     Services.getShedule(SharedPrefs().group, false).then((group) {
       setState(() {
-        _group = group;
+        _groupFalse = group;
+        _loading = false;
+      });
+    });
+    Services.getShedule(SharedPrefs().group, true).then((group) {
+      setState(() {
+        _groupTrue = group;
         _loading = false;
       });
     });
   }
 
-  //главная страница
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var test = 'Назад';
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_loading ? 'Загрузка...' : 'Расписание/четная'),
-      ),
-      body: GroupedListView<SheduleFalse, String>(
-        elements: _group,
-        groupBy: (element) => element.theDaysWeek,
-        groupComparator: (value1, value2) => value2.compareTo(value1),
-        itemComparator: (item1, item2) =>
-            item1.theDiscipline.compareTo(item2.theDiscipline),
-        order: GroupedListOrder.DESC,
-        useStickyGroupSeparators: true,
-        groupSeparatorBuilder: (String value) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            numberInText(value),
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        itemBuilder: (c, element) {
-          return Card(
-            elevation: 8.0,
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-            child: Container(
-              child: ListTile(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                leading: Icon(Icons.accessible),
-                title: Text(element.theDiscipline),
-                trailing: Icon(Icons.arrow_forward),
+      appBar: AppBar(title: Text("Расписание")),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            //четная неделя
+            Container(
+              child: GroupedListView<SheduleFalse, String>(
+                elements: _groupFalse,
+                groupBy: (element) => element.theDaysWeek,
+                groupComparator: (value1, value2) => value2.compareTo(value1),
+                itemComparator: (item1, item2) =>
+                    item1.theDiscipline.compareTo(item2.theDiscipline),
+                order: GroupedListOrder.DESC,
+                useStickyGroupSeparators: true,
+                groupSeparatorBuilder: (String value) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    numberInText(value),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                itemBuilder: (c, element) {
+                  return Card(
+                    elevation: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                    child: Container(
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        leading: Icon(Icons.accessible),
+                        title: Text(element.theDiscipline),
+                        subtitle: Text(element.theCorpus +
+                            " " +
+                            element.theTime +
+                            " " +
+                            element.theAudience +
+                            "\n" +
+                            element.theTypeExperience +
+                            " " +
+                            element.theAboutTheTeacher),
+                        trailing: Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          );
-        },
+            //Нечетная неделя
+            Container(
+              child: GroupedListView<SheduleFalse, String>(
+                elements: _groupTrue,
+                groupBy: (element) => element.theDaysWeek,
+                groupComparator: (value1, value2) => value2.compareTo(value1),
+                itemComparator: (item1, item2) =>
+                    item1.theDiscipline.compareTo(item2.theDiscipline),
+                order: GroupedListOrder.DESC,
+                useStickyGroupSeparators: true,
+                groupSeparatorBuilder: (String value) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    numberInText(value),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                itemBuilder: (c, element) {
+                  return Card(
+                    elevation: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                    child: Container(
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        leading: Icon(Icons.accessible),
+                        title: Text(element.theDiscipline),
+                        subtitle: Text(element.theCorpus +
+                            " " +
+                            element.theTime +
+                            " " +
+                            element.theAudience +
+                            "\n" +
+                            element.theTypeExperience +
+                            " " +
+                            element.theAboutTheTeacher),
+                        trailing: Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       // Меню
       drawer: getNavDrawer(context),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(title: Text('Чётная'), icon: Icon(Icons.home)),
+          BottomNavyBarItem(title: Text('Нечётная'), icon: Icon(Icons.apps)),
+        ],
+      ),
     );
   }
 }
@@ -153,4 +251,16 @@ String numberInText(String value) {
       break;
   }
   return value;
+}
+
+//функция для вычисления четной/нечетной недели
+int nowNumberWeek() {
+  int i;
+  final date = DateTime.now();
+  if (date.weekOfYear % 2 == 0) {
+    i = 0;
+  } else {
+    i = 1;
+  }
+  return i;
 }
